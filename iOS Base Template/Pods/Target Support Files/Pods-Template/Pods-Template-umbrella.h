dbe4c66c899d532b5dcab3eb9f3bce04e5a877b0
Klaus Lanzarini <klauslanza@gmail.com>
2019-06-25 09:53:12 +0200

    Fix: rename project.

--- iOS Base Template/Pods/Target Support Files/Pods-Template/Pods-Template-umbrella.h
+++ iOS Base Template/Pods/Target Support Files/Pods-Template/Pods-Template-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_iOS_Base_TemplateVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iOS_Base_TemplateVersionString[];
+FOUNDATION_EXPORT double Pods_TemplateVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TemplateVersionString[];
 
