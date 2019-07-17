b6922e8b052128f48f5b9f4361367789dbcd9f86
Klaus Lanzarini <klauslanza@gmail.com>
2019-07-17 17:31:06 +0200

    Feat: domain module.

---
+++ iOS Bootstrap/BootstrapDomain/BootstrapDomain/BootstrapDomain.h
@@ -0,0 +1,19 @@
+//
+//  BootstrapDomain.h
+//  BootstrapDomain
+//
+//  Created by Klaus Lanzarini on 17/07/2019.
+//  Copyright © 2019 Klaus Lanzarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BootstrapDomain.
+FOUNDATION_EXPORT double BootstrapDomainVersionNumber;
+
+//! Project version string for BootstrapDomain.
+FOUNDATION_EXPORT const unsigned char BootstrapDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BootstrapDomain/PublicHeader.h>
+
+
